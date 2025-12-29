e0d24a7f49b0d253644757ba195cf9516b12ff70
Nader PJR <pjrnader@gmail.com>
2025-12-29 20:27:23 +0430

    navbar created

--- app/page.js
+++ app/page.js
@@ -1,19 +1,20 @@
 import Beams from "@/components/Beams";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return <div>
-    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+    <div style={{ width: '100%', height: '100vh', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
       <Beams
         beamWidth={2}
-        beamHeight={15}
+        beamHeight={25}
         beamNumber={12}
         lightColor="#ffffff"
         speed={2}
         noiseIntensity={1.75}
         scale={0.2}
-        rotation={0}
+        rotation={30}
       />
     </div>
-    <h1>Monalux Studio </h1>
+    <Navbar />
   </div>
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,26 @@
+import GooeyNav from '@/components/GooeyNav';
+
+export default function Navbar() {
+
+    const items = [
+        { label: "Home", href: "#" },
+        { label: "About", href: "#" },
+        { label: "Contact", href: "#" },
+    ];
+
+    return <div className='relative w-full h-40 flex items-center justify-center'>
+        <div className='relative flex align-center'>
+            <GooeyNav
+                items={items}
+                particleCount={15}
+                particleDistances={[90, 10]}
+                particleR={100}
+                initialActiveIndex={0}
+                animationTime={600}
+                timeVariance={300}
+                colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+            />
+        </div>
+    </div>
+
+}
