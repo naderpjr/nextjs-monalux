2031621b58e8c5005f5b759cd9ab11391dc494aa
Nader PJR <pjrnader@gmail.com>
2025-12-29 20:03:22 +0430

    second commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
-
+import Beams from "@/components/Beams";
 
 export default function Home() {
-  return null;
+  return <div>
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <Beams
+        beamWidth={2}
+        beamHeight={15}
+        beamNumber={12}
+        lightColor="#ffffff"
+        speed={2}
+        noiseIntensity={1.75}
+        scale={0.2}
+        rotation={0}
+      />
+    </div>
+    <h1>Monalux Studio </h1>
+  </div>
 }
