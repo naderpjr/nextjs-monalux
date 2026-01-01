b230b514c023181437cac1d989ea4431a8f31282
Nader PJR <pjrnader@gmail.com>
2026-01-01 18:37:58 +0430

    about us created

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Beams from "@/components/Beams";
 import Header from "@/components/Header";
+import AboutUs1 from "@/components/mvpblocks/about-us-1";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -18,5 +19,7 @@ export default function Home() {
     </div>
     <Navbar />
     <Header />
+    <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-180 bg-linear-to-b from-transparent to-black z-0" />
+    <AboutUs1 />
   </div>
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,11 +2,11 @@
 import GradientText from '@/components/GradientText';
 
 export default function Header() {
-    return <div className='w-full h-170 flex items-center justify-center font-extrabold'>
+    return <div className='w-screen h-screen flex items-center justify-center font-extrabold'>
         <GradientText
             colors={["#0e0e0e", "#ffffff", "#0e0e0e"]}
             animationSpeed={5}
-            className="text-3xl p-4 md:text-7xl lg:text-8xl mt-60"
+            className="text-3xl p-4 md:text-7xl lg:text-8xl "
         >
             Monalux Studio
         </GradientText>
