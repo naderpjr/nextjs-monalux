fdd0b7bf41dc5ade815ae8fa98ace03f781d0b19
Nader PJR <pjrnader@gmail.com>
2026-01-02 21:26:46 +0430

    testimonial card

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Beams from "@/components/Beams";
 import Header from "@/components/Header";
 import AboutUs1 from "@/components/mvpblocks/about-us-1";
 import Faq3 from "@/components/mvpblocks/faq-3";
+import TestimonialsCarousel from "@/components/mvpblocks/testimonials-carousel";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -22,6 +23,7 @@ export default function Home() {
     <Header />
     <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-180 bg-linear-to-b from-transparent to-black z-0" />
     <AboutUs1 />
+    <TestimonialsCarousel />
     <Faq3 />
   </div>
 }
