4a553a2013b368a2c59f20687b616735531e7b1e
Nader PJR <pjrnader@gmail.com>
2026-01-07 18:35:36 +0430

    projects updated

--- components/Hero-project.jsx
+++ components/Hero-project.jsx
@@ -10,93 +10,88 @@ export const products = [
         title: "SkyCast Weather App",
         link: "https://naderpjr.github.io/SkyCast-weatherapp",
         thumbnail:
-            "/skyCast-weather-app.PNG",
+            "/skycast.png",
     },
     {
         title: "Kabul Blog",
-        link: "",
+        link: "https://kabul-blog.vercel.app/",
         thumbnail:
-            "/kabul-blog.png",
+            "/kabulblog.png",
+    },
+    {
+        title: "Inventory Management System",
+        link: "https://inventory-app-five-nu.vercel.app",
+        thumbnail:
+            "/inventory.png",
+    },
+
+    {
+        title: "Raqim Digital Marketing",
+        link: "https://naderspec.github.io/raqim/",
+        thumbnail:
+            "/raqim.png",
+    },
+    {
+        title: "Positivus Digital Marketing",
+        link: "https://positivus-kappa-plum.vercel.app",
+        thumbnail:
+            "./positivus.png",
+    },
+
+    {
+        title: "Markdown Notes App",
+        link: "https://md-notes-three.vercel.app/",
+        thumbnail:
+            "./mdnotes.png",
+    },
+    {
+        title: "Inventory Management System",
+        link: "https://inventory-app-five-nu.vercel.app",
+        thumbnail:
+            "/inventory.png",
+    },
+    {
+        title: "Kabul Blog",
+        link: "https://kabul-blog.vercel.app/",
+        thumbnail:
+            "/kabulblog.png",
+    },
+    {
+        title: "Positivus Digital Marketing",
+        link: "https://positivus-kappa-plum.vercel.app",
+        thumbnail:
+            "./positivus.png",
+    },
+
+    {
+        title: "Raqim Digital Marketing",
+        link: "https://naderspec.github.io/raqim/",
+        thumbnail:
+            "/raqim.png",
     },
     {
         title: "React Movie App",
-        link: "",
+        link: "https://react19-movie-app.netlify.app",
         thumbnail:
-            "/react-movies-thumbnail.png",
+            "/movieapp.png",
     },
 
     {
-        title: "Editorially",
-        link: "",
+        title: "Markdown Notes App",
+        link: "https://md-notes-three.vercel.app/",
         thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+            "./mdnotes.png",
     },
     {
-        title: "Editrix AI",
-        link: "",
+        title: "Raqim Digital Marketing",
+        link: "https://naderspec.github.io/raqim/",
         thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+            "/raqim.png",
     },
     {
-        title: "Pixel Perfect",
-        link: "",
+        title: "SkyCast Weather App",
+        link: "https://naderpjr.github.io/SkyCast-weatherapp",
         thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-    },
-
-    {
-        title: "Algochurn",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-    },
-    {
-        title: "Aceternity UI",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-    },
-    {
-        title: "Tailwind Master Kit",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-    },
-    {
-        title: "SmartBridge",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-    },
-    {
-        title: "Renderwork Studio",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-    },
-
-    {
-        title: "Creme Digital",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-    },
-    {
-        title: "Golden Bells Academy",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-    },
-    {
-        title: "Invoker Labs",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-    },
-    {
-        title: "E Free Invoice",
-        link: "",
-        thumbnail:
-            "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+            "/skycast.png",
     },
 ];
