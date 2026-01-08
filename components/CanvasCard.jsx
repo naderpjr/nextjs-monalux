7befcbd96be6ffb7b1fe160ae9c2a5b7a6cca31e
Nader PJR <pjrnader@gmail.com>
2026-01-08 21:55:32 +0430

    canvas updated

--- components/CanvasCard.jsx
+++ components/CanvasCard.jsx
@@ -46,12 +46,12 @@ const Card = ({
         <div
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
-            className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] relative">
-            <Icon className="absolute h-4 w-4 -top-3 -left-3 dark:text-white text-black" />
-            <Icon className="absolute h-4 w-4 -top-3 -left-3 dark:text-white text-black" />
-            <Icon className="absolute h-4 w-4 -top-3 -left-3 dark:text-white text-black" />
+            className="border border-black/20 group/canvas-card flex items-center justify-center dark:border-white/20  max-w-sm w-full mx-auto p-4 h-120 relative">
+            <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+            <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+            <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
             <Icon
-                className="absolute h-4 w-4 -top-3 -left-3 dark:text-white text-black" />
+                className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
             <AnimatePresence>
                 {hovered && (
                     <motion.div
