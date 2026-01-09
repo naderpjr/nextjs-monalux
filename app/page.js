b95bb93227b1642ca777ef72eb476f0cec62c6dd
Nader PJR <pjrnader@gmail.com>
2026-01-09 19:29:24 +0430

    Header changed

--- app/page.js
+++ app/page.js
@@ -11,30 +11,25 @@ import Faq3 from "@/components/mvpblocks/faq-3";
 import Footer4Col from "@/components/mvpblocks/footer-4col";
 import TestimonialsCarousel from "@/components/mvpblocks/testimonials-carousel";
 import Navbar from "@/components/Navbar";
+import LiquidEther from '@/components/LiquidEther';
 
 
 export default function Home() {
   return <div className="">
-    <div style={{ width: '100%', height: '100vh', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
-      <Beams
-        beamWidth={2}
-        beamHeight={25}
-        beamNumber={12}
-        lightColor="#ffffff"
-        speed={2}
-        noiseIntensity={1.75}
-        scale={0.2}
-        rotation={30}
+    <div style={{ width: '100%', height: '100vh', position: 'absolute', top: 0, left: 0, zIndex: -1, background: '#000' }}>
+      <LiquidEther
+        color0="#800020"
+        color2="#800020"
       />
     </div>
     {/* <Navbar /> */}
     <Header />
-    <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-180 bg-linear-to-b from-transparent to-black z-0" />
+    {/* <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-180 bg-linear-to-b from-transparent to-black z-0" /> */}
     <CoverText />
     <AboutUs1 />
-    <HeroProject />
+    {/* <HeroProject /> */}
     <CtaSection />
-    <CanvasCard />
+    {/* <CanvasCard /> */}
     <TestimonialsCarousel />
     <Faq3 />
     <Footer4Col />
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,13 @@
 import GradientText from '@/components/GradientText';
 
 export default function Header() {
-    return <div className='w-screen h-screen flex flex-col items-center justify-center font-medium'>
+    return <div className='w-screen h-screen flex flex-col items-center justify-center font-semibold uppercase'>
         <GradientText
             colors={["#0e0e0e", "#ffffff", "#0e0e0e"]}
             animationSpeed={5}
             className="text-3xl p-4 md:text-7xl lg:text-8xl "
         >
-            Monalux Studio
+            Monalux
         </GradientText>
     </div>
 }
